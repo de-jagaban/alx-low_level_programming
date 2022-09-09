fbd5439802814d10dc9a99d71d81335d81e9d9fb
de-jagaban <delightenyinnaya1@gmail.com>
2022-09-09 02:54:04 +0100

    Resolved answer to number 8 Advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,16 @@
-#include<stdio.h>
+#include <unistd.h>
+
 /**
- * main - main block
- * Return: 1
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return: Always 1.
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful - Dora Korpar, 2015-10-19");
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
 	return (1);
 }
