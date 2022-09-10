d473cb89faddf2c19fdc92779411bb235ed94617
de-jagaban <delightenyinnaya1@gmail.com>
2022-09-10 04:10:12 +0100

    Added answers for questions 6 to 9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0;
+ */
+int main(void)
+{
+	int b;
+
+	for (b = 0; b < 10; b++)
+	{
+		putchar(b + '0');
+
+	}
+	putchar('\n');
+
+	return (0);
+}
