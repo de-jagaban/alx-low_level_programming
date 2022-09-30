a1544b7e1b8347fafa538e19fab7e6114947219f
de-jagaban <delightenyinnaya1@gmail.com>
2022-09-30 22:36:26 +0100

    Creating a static library-question 0

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts - print a string to stdout
+ * @str: char array string type
+ * Description: Can only use _putchar
+ */
+
+void _puts(char *str)
+{
+	return (0);
+}
