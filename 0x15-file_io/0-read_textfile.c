d23979ae95eb1faa0249983d894ada1c03e97f36
de-jagaban <delightenyinnaya1@gmail.com>
2022-10-31 17:24:51 +0100

    Corrected code for question 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,39 +1,32 @@
 #include "main.h"
 
 /**
- * read_textfile - Read a text file and print to POSIX stdout
- * @filename: char string of files name
- * @letters: number of letters to read and print
- * Return: number of letters read and printed, or 0 if error
+ * read_textfile - reads a textfile
+ * @filename: file to read
+ * @letters: amount of bytes to read
+ * Return: printed letters count
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t rcount, wcount;
-	char *buffer;
+	int fd, printed, wrote;
+	char *buff;
 
+	buff = malloc(letters);
+	if (buff == NULL)
+		return (0);
 	if (filename == NULL)
 		return (0);
-
 	fd = open(filename, O_RDWR);
 	if (fd == -1)
 		return (0);
-
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	{
-		free(buffer);
+	printed = read(fd, buff, letters);
+	if (printed == -1)
 		return (0);
-	}
-	rcount = read(fd, buffer, letters);
-	if (rcount == -1)
+	wrote = write(STDOUT_FILENO, buff, printed);
+	if (wrote == -1)
 		return (0);
-
-	wcount = write(STDOUT_FILENO, buffer, rcount);
-	if (wcount == -1 || rcount != wcount)
+	if (close(fd) == -1)
 		return (0);
-	free(buffer);
-
-	close(fd);
-	return (wcount);
+	free(buff);
+	return (printed);
 }
