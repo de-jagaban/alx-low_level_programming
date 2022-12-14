3d9f71969fc610565bf05830916c7f2ddbe3d41c
de-jagaban <delightenyinnaya1@gmail.com>
2022-12-14 13:11:56 +0100

    Created the .c files and compiled them to object codes, created the libdynamic.so and 1-create_dynamic_lib.sh files

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of the string
+ * @s: char type pointer
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	return (0);
+}
---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks for aphabetical characters
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
+int _isalpha(int c)
+{
+	return (0);
+}
